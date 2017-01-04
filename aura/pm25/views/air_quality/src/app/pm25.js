a68eaaeb854ef29004a27dd1967cae22b2465863
bracken1983 <brackenbo@hotmail.com>
2017-01-04 09:44:32 +0800

    Show pm25 data with react-d3.

--- aura/pm25/views/air_quality/src/app/pm25.js
+++ aura/pm25/views/air_quality/src/app/pm25.js
@@ -9,13 +9,34 @@ export class Pm25Show extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {data: dataInit};
+    this.state = {data: dataInit, data2: dataInit};
   }
 
   setData() {
     axios.get('http://localhost:8000/api/dataShow?city=xian&startDate=20160901&endDate=201609011')
       .then(response => {
-        this.setState({data: response.data});
+        const lineData = response.data.map(element => {
+          const pm25 = Number(`${element.pm_25}`);
+          const da = new Date(`${element.local_time}`);
+          const day = Number(da);
+
+          return {x: day, y: pm25};
+        });
+
+        this.setState({data: {name: `${response.data[0].city_name}`, values: lineData}});
+      });
+
+    axios.get('http://localhost:8000/api/dataShow?city=shanghai&startDate=20160901&endDate=201609011')
+      .then(response => {
+        const lineData = response.data.map(element => {
+          const pm25 = Number(`${element.pm_25}`);
+          const da = new Date(`${element.local_time}`);
+          const day = Number(da);
+
+          return {x: day, y: pm25};
+        });
+
+        this.setState({data2: {name: `${response.data[0].city_name}`, values: lineData}});
       });
   }
 
@@ -28,6 +49,7 @@ export class Pm25Show extends Component {
       <div>
         <PM25Navbar/>
         <PM25Body bodyData={this.state.data}/>
+        <PM25Body bodyData={this.state.data2}/>
       </div>
     );
   }
--- aura/pm25/views/air_quality/src/app/pm25body.js
+++ aura/pm25/views/air_quality/src/app/pm25body.js
@@ -1,54 +1,34 @@
 import React, {Component} from 'react';
 import {Grid, Row, Col} from 'react-bootstrap';
+import {LineChart} from 'react-d3';
 
 export default class PM25Body extends Component {
-  showDataInTable() {
-    let rows = [];
-
-    const datas = this.props.bodyData;
-    console.log(datas.length);
-
-    if (datas.length === 0) {
-      rows = [];
-      return [];
-    }
-
-    for (const data of datas) {
-      rows.push(
-        <tr>
-          <td>{data.city_name}</td>
-          <td>{data.local_time}</td>
-          <td>{data.pm_25}</td>
-        </tr>
-      );
-    }
-
-    return rows;
-  }
-
   render() {
     return (
       <Grid>
         <Row className="show-grid">
-          <Col xs={12} md={3}>left</Col>
+          <Col xs={12} md={3}/>
 
           <Col xs={12} md={6}>
-            <h1>Pm25 show</h1>
 
-            <table>
-              <thead>
-                <tr>
-                  <th>city</th>
-                  <th>time</th>
-                  <th>pm25</th>
-                </tr>
-              </thead>
-
-              <tbody>{this.showDataInTable()}</tbody>
-            </table>
+            <LineChart
+              data={this.props.bodyData}
+              width="100%"
+              height={400}
+              viewBoxObject={{
+                x: 0,
+                y: 0,
+                width: 500,
+                height: 400
+              }}
+              title={this.props.bodyData.name}
+              yAxisLabel="PM25"
+              xAxisLabel="Elapsed Time (sec)"
+              domain={{x: [10], y: [-10]}}
+              />;
           </Col>
 
-          <Col xs={12} md={3}>right</Col>
+          <Col xs={12} md={3}/>
         </Row>
       </Grid>
     );
